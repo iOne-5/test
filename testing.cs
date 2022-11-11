c52201a3c03fbf013d710b83e1618c6dcf80beb3
Ivan Seleznov <ivanseleznov105@gmail.com>
2022-11-11 20:06:59 +0100

    add testing.cs new file

---
+++ testing.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Practise1
+{
+    class Program
+    {
+        public const double P1 = 3.14;
+        static void Main(string[] args)
+        {
+            string hello = "Hello world!";
+            int a = 10;
+            int b;
+            Console.WriteLine(hello);
+            a = 20;
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("pi = " + P1);
+            Console.WriteLine("Enter value for b: ");
+
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Entered value: " + b);
+
+
+
+        }
+
+    }
+
+}
